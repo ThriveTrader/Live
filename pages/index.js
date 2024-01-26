62f920f024d286a6e6ce931b9c2d8bdb5b8dce5b
ThriveTrader <156680023+ThriveTrader@users.noreply.github.com>
2024-01-26 19:51:59 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,6 @@ const Home = () => {
       <Benefits data={benefitOne} />
       </article>
       <br></br>
-      <Card />
       <br></br>
       <article className="rounded-xl border border-black w-full bggg ">
       <SectionTitle pretitle="" title="JOIN MY TELEGRAM"></SectionTitle>
