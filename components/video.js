263072c7f42a165330a8b6931d61ec015d9ce3d0
ThriveTrader <156680023+ThriveTrader@users.noreply.github.com>
2024-01-27 12:57:40 +0530

    Update video.js\

--- components/video.js
+++ components/video.js
@@ -3,34 +3,55 @@ import Container from "./container";
 
 const Video = () => {
   const [playVideo, setPlayVideo] = useState(false);
+
+  const thumbnailUrl = "URL_TO_YOUR_THUMBNAIL_IMAGE"; // Replace with the actual URL of your video thumbnail
+
   return (
     <Container>
-      <div className="w-full max-w-4xl mx-auto overflow-hidden lg:mb-20 rounded-2xl ">
+      <div className="w-full max-w-4xl mx-auto overflow-hidden lg:mb-20 rounded-2xl relative">
         <div
           onClick={() => setPlayVideo(!playVideo)}
-          className="relative bg-transparent cursor-pointer">
+          className="relative bg-transparent cursor-pointer aspect-w-16 aspect-h-9">
           {!playVideo && (
-            <button className="absolute inset-auto w-16 h-16 text-white transform -translate-x-1/2 -translate-y-1/2 lg:w-28 lg:h-28 top-1/2 left-1/2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-16 h-16  lg:w-28 lg:h-28"
-                viewBox="0 0 20 20"
-                fill="currentColor">
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">Play Video</span>
-            </button>
+            <>
+              <img
+                src={thumbnailUrl}
+                alt="Video Thumbnail"
+                className="w-full h-full object-cover"
+              />
+              <button className="absolute inset-0 flex items-center justify-center w-full h-full text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-16 h-16 lg:w-28 lg:h-28"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="sr-only">Play Video</span>
+              </button>
+            </>
           )}
           {playVideo && (
-           <iframe src="https://player.vimeo.com/video/906950726?h=ca75fc5c00" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>)}
+            <iframe
+              title="Video Player"
+              src="https://player.vimeo.com/video/906950726?h=ca75fc5c00"
+              width="640"
+              height="564"
+              frameBorder="0"
+              allow="autoplay; fullscreen"
+              allowFullScreen
+              className="absolute inset-0 w-full h-full"
+            ></iframe>
+          )}
         </div>
       </div>
     </Container>
   );
-}
+};
 
 export default Video;
